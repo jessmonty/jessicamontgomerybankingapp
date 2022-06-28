e754455c097d4db5cebb1b18bb9756edba538e97
jessmonty <89949644+jessmonty@users.noreply.github.com>
2022-06-28 17:59:54 -0600

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -28,7 +28,7 @@ function NavBar(){
             <a className="nav-link" href="#/alldata/">AllData</a>
           </li>     
           <li className="nav-item">
-            <a className = "nav-link" href="#" onClick={()=> logout()}>Logout</a>
+            <a className = "nav-link" href="#">Logout</a>
             </li>     
         </ul>
       </div>
