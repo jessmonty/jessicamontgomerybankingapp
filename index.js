a135e2396852a4ecf5eb49b2eef82938c511afe4
jessmonty <89949644+jessmonty@users.noreply.github.com>
2022-06-29 11:12:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.get('/account/all', function (req, res) {
     });
 });
 
-var port = app.listen(process.env.PORT || 3000, function(){
-    console.log('app running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`app running on port ${ PORT }`);
 });
